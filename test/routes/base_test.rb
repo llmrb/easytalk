f52f17814d6880c31245e1d3eee4cdb4b3bf9e85
Robert <0x1eef@hardenedbsd.org>
2026-03-26 07:19:34 -0300

    Add basic route tests

---
+++ test/routes/base_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+class BaseRouteTest < TestHelper
+  def test_root_path_redirects_to_sign_in
+    get "/"
+    assert_equal 302, last_response.status
+    assert_match "/sign-in", last_response.headers["Location"]
+  end
+
+  def test_health_check_returns_ok
+    get "/health"
+    assert_equal 200, last_response.status
+    assert_equal "OK", last_response.body
+  end
+
+  def test_not_found_returns_404
+    get "/nonexistent-route"
+    assert_equal 404, last_response.status
+    assert_match "Not Found", last_response.body
+  end
+end
