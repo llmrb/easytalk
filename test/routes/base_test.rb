9aa7a5dcb1dad39703038397a5f8497fa8c10672
Robert <0x1eef@hardenedbsd.org>
2026-03-26 13:03:27 -0300

    Update unknown route test to match current 404 behavior

--- test/routes/base_test.rb
+++ test/routes/base_test.rb
@@ -9,9 +9,8 @@ class BaseRouteTest < Relay::Test
     end
   end
 
-  def test_unknown_get_route_redirects_to_root
+  def test_unknown_get_route_returns_404
     get "/nonexistent-route"
-    assert_equal 302, last_response.status
-    assert_equal "/", last_response.headers["Location"]
+    assert_equal 404, last_response.status
   end
 end
